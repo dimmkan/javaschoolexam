c9b5f8c833e6c85b9fef36d61bea8cc36ba9036e
Dmitry Kanaev <dimmkan@mail.ru>
2022-02-05 08:46:06 +0300

    Fix

--- src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
+++ src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
@@ -16,31 +16,30 @@ public class PyramidBuilder {
      */
     public int[][] buildPyramid(List<Integer> inputNumbers) {
         // TODO : Implement your solution here
-        Collections.sort(inputNumbers);
-        int[][] pyramid;
-        double D = Math.sqrt(1 + 8 * inputNumbers.size());
-        try{
-        if(D > 0 && D % 1 == 0){
-            int n = (int)((D - 1) / 2);
-            pyramid = new int[n][n+(n-1)];
-            int k = 0;
-            for(int i = 0; i<n; i++) {
-                for (int j = 0; j < n + (n - 1); j++) {
-                    pyramid[i][j]=0;
+        try {
+            Collections.sort(inputNumbers);
+            int[][] pyramid;
+            double D = Math.sqrt(1 + 8 * inputNumbers.size());
+            if (D > 0 && D % 1 == 0) {
+                int n = (int) ((D - 1) / 2);
+                pyramid = new int[n][n + (n - 1)];
+                int k = 0;
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < n + (n - 1); j++) {
+                        pyramid[i][j] = 0;
+                    }
+                    int b = n - i - 1;
+                    for (int j = 0; j <= i; j++) {
+                        pyramid[i][b] = inputNumbers.get(k++);
+                        b += 2;
+                    }
                 }
-                int b = n - i - 1;
-                for (int j = 0; j <= i; j++) {
-                    pyramid[i][b]=inputNumbers.get(k++);
-                    b += 2;
-                }
+                return pyramid;
+            } else {
+                throw new CannotBuildPyramidException("Can't build pyramid");
             }
-            return pyramid;
-        }else{
+        } catch (Exception e) {
             throw new CannotBuildPyramidException("Can't build pyramid");
         }
-        }catch (Exception e){
-            throw new CannotBuildPyramidException("Can't build pyramid");
-        }
-        
     }
 }
